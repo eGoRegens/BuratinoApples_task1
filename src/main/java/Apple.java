17e4ca2eaff78d6cbcedf816faa1c22014effbc1
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 22:55:59 +0400

    назначение констант

--- src/main/java/Apple.java
+++ src/main/java/Apple.java
@@ -1,11 +1,11 @@
 public class Apple {
     public static void main(String[] args) {
-        int applesBuranino = 5;
-        int applesTookKarabas = 2;
-        int residualApplesBuratino = applesBuranino - applesTookKarabas;
+        final int APPLES_BURATINO = 5;
+        final int APPLES_TOOK_KARABAS = 2;
+        final int RESIDUAL_APPLES_BURATINO = APPLES_BURATINO - APPLES_TOOK_KARABAS; //остаток яблок
 
-        System.out.println("У Буратино изначально было " + applesBuranino + " яблок.");
-        System.out.println("Карабас Барабас отобрал у него " + applesTookKarabas + " яблок.");
-        System.out.println(residualApplesBuratino + " яблок осталось у Буратино.");
+        System.out.println("У Буратино изначально было " + APPLES_BURATINO + " яблок.");
+        System.out.println("Карабас Барабас отобрал у него " + APPLES_TOOK_KARABAS + " яблок.");
+        System.out.println(RESIDUAL_APPLES_BURATINO + " яблок осталось у Буратино.");
     }
 }
