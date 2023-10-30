3832ebb33ed2db06daf4aaa9c1016d2e51e34da4
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 19:06:27 +0400

    gitignore+задача

---
+++ src/main/java/Apple.java
@@ -0,0 +1,11 @@
+public class Apple {
+    public static void main(String[] args) {
+        int applesBuranino = 5;
+        int applesTookKarabas = 2;
+        int residualApplesBuratino = applesBuranino - applesTookKarabas;
+
+        System.out.println("У Буратино изначально было " + applesBuranino + " яблок.");
+        System.out.println("Карабас Барабас отобрал у него " + applesTookKarabas + " яблок.");
+        System.out.println(residualApplesBuratino + " яблок(а) осталось у Буратино.");
+    }
+}
