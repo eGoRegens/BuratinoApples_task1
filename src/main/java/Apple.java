5a4a1ece973eed327f6001aeadd109497d62fccc
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-31 19:40:30 +0400

    исправление ошибок

--- src/main/java/Apple.java
+++ src/main/java/Apple.java
@@ -1,11 +1,11 @@
 public class Apple {
     public static void main(String[] args) {
-        final int APPLES_BURATINO = 5;
-        final int APPLES_TOOK_KARABAS = 2;
-        final int RESIDUAL_APPLES_BURATINO = APPLES_BURATINO - APPLES_TOOK_KARABAS; //остаток яблок
+        final int applesBuranino = 5;
+        final int applesTookKarabas = 2;
+        final int residualApplesBuratino = applesBuranino - applesTookKarabas; //остаток яблок
 
-        System.out.println("У Буратино изначально было " + APPLES_BURATINO + " яблок.");
-        System.out.println("Карабас Барабас отобрал у него " + APPLES_TOOK_KARABAS + " яблок.");
-        System.out.println(RESIDUAL_APPLES_BURATINO + " яблок осталось у Буратино.");
+        System.out.println("У Буратино изначально было " + applesBuranino + " яблок.");
+        System.out.println("Карабас Барабас отобрал у него " + applesTookKarabas + " яблок.");
+        System.out.println(residualApplesBuratino + " яблок(а) осталось у Буратино.");
     }
 }
