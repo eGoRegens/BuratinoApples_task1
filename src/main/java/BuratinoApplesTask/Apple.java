220f39b58ffffe24a8816a3707040616eee23160
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-11-01 16:24:46 +0400

    исправление ошибок

---
+++ src/main/java/BuratinoApplesTask/Apple.java
@@ -0,0 +1,13 @@
+package BuratinoApplesTask;
+
+public class Apple {
+    public static void main(String[] args) {
+        final int applesBuranino = 5;
+        final int applesTookKarabas = 2;
+        final int residualApplesBuratino = applesBuranino - applesTookKarabas; //остаток яблок
+
+        System.out.println("У Буратино изначально было " + applesBuranino + " яблок.");
+        System.out.println("Карабас Барабас отобрал у него " + applesTookKarabas + " яблок.");
+        System.out.println(residualApplesBuratino + " яблок(а) осталось у Буратино.");
+    }
+}
