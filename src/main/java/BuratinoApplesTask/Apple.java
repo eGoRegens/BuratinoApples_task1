43903fbc214cd3deddf1c8fa682be5684277433d
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-11-01 16:37:42 +0400

    разделение переменных

--- src/main/java/BuratinoApplesTask/Apple.java
+++ src/main/java/BuratinoApplesTask/Apple.java
@@ -4,7 +4,8 @@ public class Apple {
     public static void main(String[] args) {
         final int applesBuranino = 5;
         final int applesTookKarabas = 2;
-        final int residualApplesBuratino = applesBuranino - applesTookKarabas; //остаток яблок
+
+        final int residualApplesBuratino = applesBuranino - applesTookKarabas; //остаток
 
         System.out.println("У Буратино изначально было " + applesBuranino + " яблок.");
         System.out.println("Карабас Барабас отобрал у него " + applesTookKarabas + " яблок.");
